207018218d525da2226882a83d77261c1d598da1
DmitryS93 <119356125+DmitryS93@users.noreply.github.com>
2023-02-22 16:08:10 +0300

    task execution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,27 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.root = null;
+    }
 
     setRange(min, max) {
-
+        this.minVal = min;
+        this.maxVal = max;
+        return this.minVal, this.maxVal
     }
 
     guess() {
-
+        let attempt = Math.round((this.minVal + (this.maxVal-this.minVal)/2));
+        return attempt;
     }
 
     lower() {
-
+        this.maxVal = Math.round((this.minVal + (this.maxVal-this.minVal)/2))
+        return this.maxVal
     }
 
     greater() {
-
+        this.minVal = Math.round((this.minVal + (this.maxVal-this.minVal)/2));
+        return this.minVal
     }
 }
 
